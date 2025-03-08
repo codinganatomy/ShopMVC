9203aa392a7f8dfa9d1f75753bc58a491fea3a3b
Md. Abul Kalam <akazad.cse13@gmail.com>
2025-03-08 17:10:53 +0600

    Initial changes

---
+++ ShopMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/5
+        public IActionResult Welcome(int Id, string name, int age)
+        {
+            ViewData["name"] = "Hello " + name;
+            ViewBag.Age = age;
+            ViewBag.Id = Id;
+            return View();
+        }
+    }
+}
